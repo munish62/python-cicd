b4aac58212ae4e53516a14fd4e0564ebd24b2078
munish62 <68247522+munish62@users.noreply.github.com>
2024-01-17 22:57:10 -0500

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,14 +1,4 @@
-# a=["abc","bcd","cde","def"]
-# b=[55,50,23,20]
-
-# family={}
-
-# for i in range(len(a)):
-#   family[a[i]] = b[i]
-    
-# print(family) """
 def func(x):
     return x + 1
-    
 def test_answer():
     assert func(3) == 5
